21f08387061c32624ad59c8ee93b67abe7513119
BirLukas <cluks@seznam.cz>
2026-06-05 09:08:33 +0200

    Book needs to be read once, not every day

--- Assets/Scripts/BookUnlockDoor.cs
+++ Assets/Scripts/BookUnlockDoor.cs
@@ -9,6 +9,20 @@ public class BookUnlockDoor : MonoBehaviour
 
     private bool unlocked = false;
 
+    void Start()
+    {
+        if (GameProgressionManager.Instance != null && GameProgressionManager.Instance.currentDay > 1)
+        {
+            unlocked = true;
+
+            if (lightToTurnOff != null)
+                lightToTurnOff.enabled = false;
+
+            if (door != null)
+                door.UnlockDoor();
+        }
+    }
+
     void Update()
     {
         if (!unlocked && book != null && book.HasBeenReadOnce)
